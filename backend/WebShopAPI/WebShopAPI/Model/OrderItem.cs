f8f47344b586c7707eb7203df8c56cd16a122ff0
Ati <nagyonkellvalamihez@gmail.com>
2023-11-15 13:34:00 +0100

    OrderItem modell created

--- backend/WebShopAPI/WebShopAPI/Model/OrderItem.cs
+++ backend/WebShopAPI/WebShopAPI/Model/OrderItem.cs
@@ -2,5 +2,18 @@
 {
     public class OrderItem
     {
+        public int OrderItemId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
     }
 }
