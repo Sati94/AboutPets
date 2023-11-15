20162b49a4c8df17e8593ad46ae46d296b154e74
Ati <nagyonkellvalamihez@gmail.com>
2023-11-15 13:45:41 +0100

    User modell implemented

--- backend/WebShopAPI/WebShopAPI/Model/OrderItem.cs
+++ backend/WebShopAPI/WebShopAPI/Model/OrderItem.cs
@@ -14,7 +14,6 @@ namespace WebShopAPI.Model
 
         public int UserId { get; set; }
         public User User { get; set; }
-        public int ProductId { get; set; }
         public Product Product { get; set; }
 
     }
--- backend/WebShopAPI/WebShopAPI/Model/User.cs
+++ backend/WebShopAPI/WebShopAPI/Model/User.cs
@@ -2,5 +2,15 @@
 {
     public class User
     {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }  
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public decimal Bonus { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
     }
 }
