da79f1c74bad0c1366647fdc13721454ce7a1b96
Ati <nagyonkellvalamihez@gmail.com>
2023-11-15 13:31:12 +0100

    Product modell created, add orderItem

--- backend/WebShopAPI/WebShopAPI/Model/Product.cs
+++ backend/WebShopAPI/WebShopAPI/Model/Product.cs
@@ -1,6 +1,17 @@
-﻿namespace WebShopAPI.Model
+﻿using WebShopAPI.Model.CategoryClasses;
+
+namespace WebShopAPI.Model
 {
     public class Product
     {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public Category Category { get; set; }
+        public SubCategory SubCategory { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
     }
 }
