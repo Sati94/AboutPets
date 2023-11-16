9a111e5ebdf43ef7952afc81fdfbde999a836285
Ati <nagyonkellvalamihez@gmail.com>
2023-11-16 11:11:18 +0100

    Refactored UserProfile

--- backend/WebShopAPI/WebShopAPI/Model/UserModels/UserProfile.cs
+++ backend/WebShopAPI/WebShopAPI/Model/UserModels/UserProfile.cs
@@ -2,5 +2,14 @@
 {
     public class UserProfile
     {
+        public string UserProfileId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public decimal Bonus { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
