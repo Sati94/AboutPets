3a26ac199ea43fb83993f6576a9097ba1536f5c8
Ati <nagyonkellvalamihez@gmail.com>
2023-11-20 14:30:38 +0100

    add RegistrationResponse

---
+++ backend/WebShopAPI/WebShopAPI/Contracts/RegistrationResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebShopAPI.Contracts
+{
+    public record RegistrationResponse(
+        [Required] string Email,
+        [Required] string UserName);
+    
+    
+}
