216ecda6d9ba15a4d4de6452ff9dabc42627febf
Ati <nagyonkellvalamihez@gmail.com>
2023-11-20 15:25:05 +0100

    Add AuthContorller + Register with metods

---
+++ backend/WebShopAPI/WebShopAPI/Controllers/AuthContorller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShopAPI.Data;
+using WebShopAPI.Service.Authentication;
+
+namespace WebShopAPI.Controllers
+{
+    public class AuthContorller : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        private readonly WebShopContext _webShopcontext;
+
+  
+    }
+}
