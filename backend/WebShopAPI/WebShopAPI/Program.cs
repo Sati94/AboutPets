2f61737dc11853331a657a8bf23aa34450d49e5c
Ati <nagyonkellvalamihez@gmail.com>
2023-11-20 15:13:04 +0100

    Refactored Program.cs + AddIdentity,AddRole,AddScope(IToken,IAuth),add ConfigureSwagger.

--- backend/WebShopAPI/WebShopAPI/Program.cs
+++ backend/WebShopAPI/WebShopAPI/Program.cs
@@ -1,39 +1,170 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using WebShopAPI.Service.Authentication;
+using WebShopAPI.Model.UserModels;
+using System.Numerics;
+using System.Text;
 using WebShopAPI.Data;
 
-namespace WebShopAPI
+
+
+var builder = WebApplication.CreateBuilder(args);
+AddAuthentication();
+AddIdentity();
+ConfigureSwagger();
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddTransient<ITokenService, TokenService>();
+builder.Services.AddDbContext<WebShopContext>();
+
+var app = builder.Build();
+AddRoles();
+AddAdmin();
+            
+if (app.Environment.IsDevelopment())
+   {
+     app.UseSwagger();
+     app.UseSwaggerUI();
+   }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
+        
+void AddAuthentication()
 {
-    public class Program
+    builder.Services
+       .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+       .AddJwtBearer(options =>
+       {
+           options.TokenValidationParameters = new TokenValidationParameters()
+           {
+               ClockSkew = TimeSpan.Zero,
+               ValidateIssuer = true,
+               ValidateAudience = true,
+               ValidateLifetime = true,
+               ValidateIssuerSigningKey = true,
+               ValidIssuer = "apiWithAuthBackend",
+               ValidAudience = "apiWithAuthBackend",
+               IssuerSigningKey = new SymmetricSecurityKey(
+               Encoding.UTF8.GetBytes("!SomethingSecret!")
+               ),
+           };
+       });
+}
+void AddIdentity()
+{
+    builder.Services
+    .AddIdentityCore<IdentityUser>(options =>
     {
-        public static void Main(string[] args)
+        options.SignIn.RequireConfirmedAccount = false;
+        options.User.RequireUniqueEmail = true;
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<WebShopContext>();
+}
+void ConfigureSwagger()
+{
+    builder.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<WebShopContext>();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                     {
+                   {
+                                 new OpenApiSecurityScheme
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+});
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
+    });
 
 
-            app.MapControllers();
+}
+void AddRoles()
+{
+    using var scope = app.Services.CreateScope(); // RoleManager is a scoped service, therefore we need a scope instance to access it
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-            app.Run();
+    var tAdmin = CreateAdminRole(roleManager);
+    tAdmin.Wait();
+
+    var tUser = CreateUserRole(roleManager);
+    tUser.Wait();
+}
+async Task CreateAdminRole(RoleManager<IdentityRole> roleManager)
+{
+    await roleManager.CreateAsync(new IdentityRole("Admin")); //The role string should better be stored as a constant or a value in appsettings
+}
+
+async Task CreateUserRole(RoleManager<IdentityRole> roleManager)
+{
+    await roleManager.CreateAsync(new IdentityRole("User")); //The role string should better be stored as a constant or a value in appsettings
+}
+void AddAdmin()
+{
+    var tAdmin = CreateAdminIfNotExists();
+    tAdmin.Wait();
+}
+
+async Task CreateAdminIfNotExists()
+{
+    using var scope = app.Services.CreateScope();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var adminInDb = await userManager.FindByEmailAsync("admin@admin1.com");
+    if (adminInDb == null)
+    {
+        var admin = new IdentityUser { UserName = "admin1", Email = "admin@admin1.com" };
+        var adminCreated = await userManager.CreateAsync(admin, "admin1234");
+
+        if (adminCreated.Succeeded)
+        {
+            await userManager.AddToRoleAsync(admin, "Admin");
+            var customUser = new User
+            {
+                UserName = admin.UserName,
+                Email = admin.Email,
+                UserId = int.Parse(admin.Id)
+            };
+            using var dbContext = scope.ServiceProvider.GetRequiredService<WebShopContext>();
+            dbContext.Users.Add(customUser);
+            await dbContext.SaveChangesAsync();
+
         }
     }
+
+
 }
+
+
