d4f68a40ce45f17f1f4c0649489ec1115797e074
Ati <nagyonkellvalamihez@gmail.com>
2023-11-20 14:29:13 +0100

    add Contracts map and add RegistrationRequest

---
+++ backend/WebShopAPI/WebShopAPI/Contracts/RegistrationRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebShopAPI.Contracts
+{
+    public record RegistrationRequest(
+        [Required] string Email,
+        [Required] string UserName,
+        [Required] string Password);
+    
+        
+    
+}
--- backend/WebShopAPI/WebShopAPI/Model/UserModels/User.cs
+++ backend/WebShopAPI/WebShopAPI/Model/UserModels/User.cs
@@ -3,12 +3,13 @@ using WebShopAPI.Model.OrderModel;
 
 namespace WebShopAPI.Model.UserModels
 {
-    public class User 
+    public class User : IdentityUser
     {
         public int UserId { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+     
 
 
         public List<OrderItem> OrderItems { get; set; }
