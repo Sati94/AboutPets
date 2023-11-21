6fb4e5ce00bde64a51566f41e506835c2728f601
Ati <nagyonkellvalamihez@gmail.com>
2023-11-21 15:16:40 +0100

    add ProductController with CreateProduct method

---
+++ backend/WebShopAPI/WebShopAPI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebShopAPI.Model;
+using WebShopAPI.Service.ProductServiceMap;
+
+namespace WebShopAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpPost("/create/product"), Authorize(Roles ="Admin")]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            var newProduct = _productService.CreatePorductAsync(product);
+            return Ok(newProduct);
+        }
+
+    }
+}
