10ae53c77d5fcf6b9316f7e50168d4127c04b934
Ati <nagyonkellvalamihez@gmail.com>
2023-12-06 11:01:24 +0100

    add OrderControllerTest.cs, add GetAll_Order_Return_NotNull() Test

---
+++ backend/WebShopAPI/WebShopApiTest/IntegrationTest/OrderControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopApiTest.IntegrationTest
+{
+    public class OrderControllerTest : WebApplicationFactory<Program>
+    {
+        private  HttpClient _httpClient;
+        private  WebShopContext _webShopContext;
+        private IAuthService _authService;
+        [SetUp]
+        public void Setup()
+        {
+            string connection = "Server=localhost,1433;Database=PetProject;User Id=sa;Password=SaraAttila1994;Encrypt=True;TrustServerCertificate=True;";
+            Environment.SetEnvironmentVariable("CONNECTION_STRING", connection);
+            var dbConnection = new DbContextOptionsBuilder<WebShopContext>()
+            .UseSqlServer(connection)
+                .Options;
+            _webShopContext = new WebShopContext(dbConnection);
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            _httpClient = CreateClient();
+            AuthRequest authRequest = new AuthRequest("admin@admin.com", "admin1234");
+            string jsonString = JsonSerializer.Serialize(authRequest);
+            StringContent jsonStringContent = new StringContent(jsonString);
+            jsonStringContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var response = _httpClient.PostAsync("/Login", jsonStringContent).Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+            var desContent = JsonSerializer.Deserialize<AuthResponse>(content, options);
+            var token = desContent.Token;
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+        }
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            CleanUpDate();
+            _httpClient.Dispose();
+        }
+        private void CleanUpDate()
+        {
+            var options = new DbContextOptionsBuilder<WebShopContext>()
+                 .UseInMemoryDatabase(databaseName: "TestDatabase")
+                 .Options;
+
+            var productsToDelete = _webShopContext.Products.Where(p => p.ProductName.Contains("Test")).ToList();
+
+            _webShopContext.Products.RemoveRange(productsToDelete);
+            _webShopContext.SaveChanges();
+        }
+        [Test]
+        public async Task GetAll_Order_Return_NotNull()
+        {
+            var response = await _httpClient.GetAsync("/orderlist/all");
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.IsNotNull(content);
+            Assert.IsNotEmpty(content);
+        }
+    }
+}
